3b9f02d97b4404bf45b00fb418d16ddc78174e05
Derek Barber <db@derekbarber.ca>
2013-05-31 11:32:23 -0700

    Add a Site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Enegotiation::Application.routes.draw do
+  get "site/home"
+  get "site/about"
+  get "site/team"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
